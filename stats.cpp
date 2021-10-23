eece16bf73e5ec6b797dfac5a384e57e02dcb023
waseem-syed <91089691+waseem-syed@users.noreply.github.com>
2021-10-23 18:47:20 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,17 @@
 #include "stats.h"
+#include<algorithm>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Stats Statistics::computeStatistics(const std::vector<float>& scoreVector)
+{
+	Stats computedStats(*scoreVector.begin(), *scoreVector.begin(), *scoreVector.begin());
+
+	float sum = 0;
+	for(unsigned int it = 0; it < scoreVector.size() ; it++)
+	{
+		sum += scoreVector[it];
+		computedStats.m_max = std::max(computedStats.m_max, scoreVector[it]);
+		computedStats.m_min = std::min(computedStats.m_min, scoreVector[it]);
+	}
+	computedStats.m_average = sum / scoreVector.size();
+	return computedStats;
 }
