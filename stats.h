918a5ba384bfbc57635e3eb7d7e6694f954ab54c
waseem-syed <91089691+waseem-syed@users.noreply.github.com>
2021-10-23 18:46:11 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,29 @@
 #include <vector>
+#include <cmath>
 
-namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
-}
+struct Stats
+{
+	float m_average;
+	float m_max;
+	float m_min;
+
+   Stats()
+   {
+   }
+    
+   Stats(float average,	float max, float min)
+   : m_average(average)
+   , m_max(max)
+   , m_min(min)
+   {
+   }
+};
+
+class Statistics : public Stats
+{
+public:
+	Statistics()
+	{
+	}
+	Stats computeStatistics(const std::vector<float>& scoreVector);
+};
